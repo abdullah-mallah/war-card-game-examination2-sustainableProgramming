254a2f0cb5616ede6ff55ee466439013a0772434
unknown <abdullahmallah2000@gmail.com>
2023-02-14 17:56:46 +0100

    four classes in folder Classes was created to be used later on

---
+++ Classes/inputOutput.py
@@ -0,0 +1,2 @@
+class inOut:
+    pass
---
+++ Classes/main.py
@@ -0,0 +1,10 @@
+from player import Player
+from inputOutput import inOut
+from fileReaderWriter import FileRW
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
