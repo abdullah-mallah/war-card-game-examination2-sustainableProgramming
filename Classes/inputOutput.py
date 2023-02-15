40fd0fffb7cd7ebd9a317db0ff65f9d13e0a9a85
Hampus Gunnarsson <hampus.gunnarsson0053@stud.hkr.se>
2023-02-15 17:01:25 +0100

    Method called brain was created which calls methods to read the choice of the user whether he is going to play vs computer or vs another player. According to that it will decide to read one name or two names

--- Classes/inputOutput.py
+++ Classes/inputOutput.py
@@ -1,3 +1,30 @@
-class inOut:
+class Input_output:
     def __init__(self) -> None:
         self._input_choice_lvl1 = ""
+        self._input_choice_name_player1 = ""
+        self._input_choice_name_player2 = "Computer"
+          
+    def brain(self):
+        self.lvl1()
+        self.lvl1_input()
+        if self._input_choice_lvl1 == "1":
+            self.lvl2_choice1_input()
+        else:
+            self.lvl2_choice2_input()
+
+    def lvl1(self):
+        print("1) VS. Computer")
+        print("2) Two players")
+
+
+    def lvl1_input(self):
+        self._input_choice_lvl1 = input("Choice: ")
+
+
+    def lvl2_choice1_input(self):
+        self._input_name_player1 = input("Name of the first player: ")
+
+
+    def lvl2_choice2_input(self):
+        self._input_name_player1 = input("Name of the first player: ")
+        self._input_name_player2 = input("Name of the second player: ")
