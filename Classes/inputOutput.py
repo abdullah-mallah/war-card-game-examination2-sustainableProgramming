4c0499720ace957ed589f944603779a0db995ac4
Hampus Gunnarsson <hampus.gunnarsson0053@stud.hkr.se>
2023-02-15 17:33:43 +0100

    Changed a variable name in Input_output class

--- Classes/inputOutput.py
+++ Classes/inputOutput.py
@@ -1,13 +1,13 @@
 class Input_output:
     def __init__(self) -> None:
-        self._input_choice_lvl1 = ""
+        self._choice_lvl1 = ""
         self._name_player1 = ""
         self._name_player2 = "Computer"
 
     def brain(self):
         self.lvl1()
         self.lvl1_input()
-        if self._input_choice_lvl1 == "1":
+        if self._choice_lvl1 == "1":
             self.lvl2_choice1_input()
         else:
             self.lvl2_choice2_input()
@@ -17,7 +17,7 @@ class Input_output:
         print("2) Two players")
 
     def lvl1_input(self):
-        self._input_choice_lvl1 = input("Choice: ")
+        self._choice_lvl1 = input("Choice: ")
 
     def lvl2_choice1_input(self):
         self._name_player1 = input("Name of the first player: ")
