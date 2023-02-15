696305d461d7971319f4975546520aafbe362b4d
Hampus Gunnarsson <hampus.gunnarsson0053@stud.hkr.se>
2023-02-15 17:45:06 +0100

    Comments added to class Input_output

--- Classes/inputOutput.py
+++ Classes/inputOutput.py
@@ -4,6 +4,9 @@ class Input_output:
         self._name_player1 = ""
         self._name_player2 = "Computer"
 
+    # This will print out level 1 choices
+    # Read choice of level 1
+    # and decide if its going to read the name of one player or two players
     def brain(self):
         self.lvl1()
         self.lvl1_input()
@@ -12,16 +15,20 @@ class Input_output:
         else:
             self.lvl2_choice2_input()
 
+    # Print out the choice of level 1
     def lvl1(self):
         print("1) VS. Computer")
         print("2) Two players")
 
+    # Read the choice of the user of level 1
     def lvl1_input(self):
         self._choice_lvl1 = input("Choice: ")
 
+    # Read the name of one player
     def lvl2_choice1_input(self):
         self._name_player1 = input("Name of the first player: ")
 
+    # Read the names of two players
     def lvl2_choice2_input(self):
         self._name_player1 = input("Name of the first player: ")
         self._name_player2 = input("Name of the second player: ")
