cd677c73dc447582089130c6ee96ebb66d839eb9
abdullah <abdullahmallah2000@gmail.com>
2023-02-16 13:06:44 +0100

    three empty methods add to class file_reader_writer

--- Classes/fileReaderWriter.py
+++ Classes/fileReaderWriter.py
@@ -1,2 +1,10 @@
 class FileRW:
-    pass
+    def __init__(self, file_name):
+        self._file_name = file_name
+
+    def check_name(self) -> bool:
+        name_found = False
+        return name_found
+
+    def store_name(self, name):
+        pass
