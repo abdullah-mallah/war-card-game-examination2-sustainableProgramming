5398128c170e02c0c0f2a56eae16c8fb3e34195e
abdullah <abdullahmallah2000@gmail.com>
2023-02-16 10:11:15 +0100

    constructor in player class was created and two objects of the player class was created inside the main class

--- Classes/main.py
+++ Classes/main.py
@@ -8,6 +8,15 @@ def main():
     # will take the choice vs_computer or not and the name of player/s
     inputOutput.brain()
 
+    # will creat players according to the choice of the user at main menue
+    for player in range(1, 3):
+        if player == 1:
+            player1 = Player(inputOutput.get_name1(), player)
+        else:
+            if inputOutput.get_choice_lvl1() == "1":
+                player2 = Player("computer", player)
+            else:
+                player2 = Player(inputOutput.get_name2(), player)
 
 if __name__ == "__main__":
     main()
--- Classes/player.py
+++ Classes/player.py
@@ -1,2 +1,10 @@
 class Player:
-    pass
+
+    def __init__(self, name, player):
+        self._name = name
+        if player == 1:
+            self._deck = self.create_deck1()
+            self._deck1_used = True
+        else:
+            self._deck = self.create_deck2()
+        self._temp_deck = []
