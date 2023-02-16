770fe7cc7bbff2d3effbf577e05ce8b5ff904298
abdullah <abdullahmallah2000@gmail.com>
2023-02-16 10:45:59 +0100

    inside main class 4 variables was created and two loops was created the outer loop is responsible to check if a winner found or not and the inner loop is going to check the turn of the player

--- Classes/main.py
+++ Classes/main.py
@@ -18,5 +18,29 @@ def main():
             else:
                 player2 = Player(inputOutput.get_name2(), player)
 
+    winner_found = False
+    cards_found = True
+    war_card1 = 0
+    war_card2 = 0
+
+    # will keep looping untill one player has no more cards
+    while not winner_found:
+        # loop on turns of players to show thier hands
+        for turn in range(1, 3):
+            inputOutput.lvl_game_choices()
+            inputOutput.lvl_game_input()
+            if inputOutput.get_input_lvl_game() == "1" and turn == 1:
+                # check if the player has cards in his deck
+                cards_found = player1.check_cards_left(player1.get_card_list())
+                if cards_found:
+                    war_card1 = player1.get_next_card()
+                    inputOutput.lvl_game_score(war_card1)
+                # if no cards with first player
+                else:
+                    winner_found = True
+                    inputOutput.congrats(turn + 1)
+                    break
+
+
 if __name__ == "__main__":
     main()
