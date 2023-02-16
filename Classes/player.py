71814ef4215c2324e4d6270e86727543385895e9
abdullah <abdullahmallah2000@gmail.com>
2023-02-16 11:26:33 +0100

    inside player class a method to take a card from the player hand and put it on the floor was created

--- Classes/player.py
+++ Classes/player.py
@@ -11,18 +11,21 @@ class Player:
             self._deck = self.create_deck2()
         self._temp_deck = []
 
+    # Creating a deck for player 1
     def create_deck1(self):
         deck = []
         deck = self.create_deck_clubs() + self.create_deck_diamonds()
         random.shuffle(deck)
         return deck
 
+    # Creating a deck for player 2
     def create_deck2(self):
         deck = []
         deck = self.create_deck_hearts() + self.create_deck_spades()
         random.shuffle(deck)
         return deck
 
+    # Creating a club deck
     def create_deck_clubs(self):
         type = "clubs"
         deck_clubs = []
@@ -30,6 +33,7 @@ class Player:
             deck_clubs.append([type, number])
         return deck_clubs
 
+    # Creating a diamond deck
     def create_deck_diamonds(self):
         type = "diamonds"
         deck_diamond = []
@@ -37,6 +41,7 @@ class Player:
             deck_diamond.append([type, number])
         return deck_diamond
 
+    # Creating a heart deck
     def create_deck_hearts(self):
         type = "hearts"
         deck_hearts = []
@@ -44,6 +49,7 @@ class Player:
             deck_hearts.append([type, number])
         return deck_hearts
 
+    # Creating a spade deck
     def create_deck_spades(self):
         type = "spades"
         deck_spades = []
@@ -51,11 +57,23 @@ class Player:
             deck_spades.append([type, number])
         return deck_spades
 
+    # Check if the player has cards in his hand
     def check_cards_left(self, cards_list):
         cards_left = False
         if len(cards_list) > 0:
             cards_left = True
         return cards_left
 
+    # It will take one card from the hand deck and add it to the floor deck
+    def get_next_card(self):
+        self._temp_deck.append(self._deck[0])
+        del self._deck[0]
+        # card is a list of two values, type and number
+        for card in self._temp_deck:
+            # check if the list is in the last place of the floor list
+            if card == self._temp_deck[len(self._temp_deck) - 1]:
+                return card[1]  # return only the number of the card
+
+    # Return the cards of the player
     def get_card_list(self):
         return self._deck
