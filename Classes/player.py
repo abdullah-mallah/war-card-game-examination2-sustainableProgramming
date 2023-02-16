64f414d99c935864549246625d3fc6d8ff8d1573
abdullah <abdullahmallah2000@gmail.com>
2023-02-16 10:26:26 +0100

    methods to create deck 1 for player 1 was created in player class

--- Classes/player.py
+++ Classes/player.py
@@ -1,10 +1,32 @@
+import random
+
+
 class Player:
 
     def __init__(self, name, player):
         self._name = name
         if player == 1:
             self._deck = self.create_deck1()
-            self._deck1_used = True
         else:
             self._deck = self.create_deck2()
         self._temp_deck = []
+
+    def create_deck1(self):
+        deck = []
+        deck = self.create_deck_clubs() + self.create_deck_diamonds()
+        random.shuffle(deck)
+        return deck
+
+    def create_deck_clubs(self):
+        type = "clubs"
+        deck_clubs = []
+        for number in range(1, 14):
+            deck_clubs.append([type, number])
+        return deck_clubs
+
+    def create_deck_diamonds(self):
+        type = "diamonds"
+        deck_diamond = []
+        for number in range(1, 14):
+            deck_diamond.append([type, number])
+        return deck_diamond
