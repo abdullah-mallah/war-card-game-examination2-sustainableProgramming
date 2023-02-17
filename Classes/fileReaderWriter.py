832fb7817510a58bebf76791b0bf147bfb3be387
Eszter Kalmár0005 <eszter.kalmar0005@stud.hkr.se>
2023-02-17 10:34:08 +0100

    in class readerwriter a method to check if the name exists in the file was created

--- Classes/fileReaderWriter.py
+++ Classes/fileReaderWriter.py
@@ -2,8 +2,19 @@ class FileRW:
     def __init__(self, file_name):
         self._file_name = file_name
 
-    def check_name(self) -> bool:
+    # Check whether the name is already in the file
+    def check_name(self, name) -> bool:
         name_found = False
+        more_enteries = True
+        with open(self._file_name, 'r') as rf:
+            while more_enteries:
+                line = rf.readline().rstrip('\n')
+                if line != '':
+                    if line == name:
+                        name_found = True
+                        more_enteries = False
+                else:
+                    more_enteries = False
         return name_found
 
     def store_name(self, name):
