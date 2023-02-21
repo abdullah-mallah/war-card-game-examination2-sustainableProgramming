bf23dc2c90bdfc82a9316c745f7482e99f7e3542
Eszter Kalmár0005 <eszter.kalmar0005@stud.hkr.se>
2023-02-21 19:57:15 +0100

    class readerwriter was updated to v4

--- Classes/fileReaderWriter.py
+++ Classes/fileReaderWriter.py
@@ -5,17 +5,23 @@ class FileRW:
     # Check whether the name is already in the file
     def check_name(self, name) -> bool:
         name_found = False
-        names = self.get_names()
-        for line in names:
-            if line[0] == name:
-                name_found = True
+        more_enteries = True
+        with open(self._file_name, 'r') as rf:
+            while more_enteries:
+                line = rf.readline().rstrip('\n')
+                if line != '':
+                    if line == name:
+                        name_found = True
+                        more_enteries = False
+                else:
+                    more_enteries = False
         return name_found
 
     # Store the players name and score in a text file
-    def store_name(self, name, score):
+    def store_name(self, name, wins, times_played):
         if not self.check_name(name):
             with open(self._file_name, 'a') as wf:
-                wf.write(name + "," + score + "\n")
+                wf.write(name + "," + wins + "," + times_played + "\n")
 
     # Retrieving names and score from the text file
     def get_names(self):
@@ -32,20 +38,27 @@ class FileRW:
                     more_enteries = False
         return names
 
-    def update_wins(self, name, wins):
+    def update_wins(self, name, wins, times_played):
         names = self.get_names()
         for line in names:
             if line[0] == name:
                 line[1] = wins  # Updating the score of the player
+                line[2] = times_played
         self.store_names(names)
 
     def store_names(self, names):
         with open(self._file_name, "w") as wf:
             for name in names:
-                wf.write(name[0] + "," + name[1] + "\n")
+                wf.write(name[0] + "," + name[1] + "," + name[2] + "\n")
 
     def get_wins(self, name):
         names = self.get_names()
         for line in names:
             if line[0] == name:
                 return line[1]
+
+    def get_times_played(self, name):
+        names = self.get_names()
+        for line in names:
+            if line[0] == name:
+                return line[2]
